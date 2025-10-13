1767cfae00178d1285f77244695ee29b7ef661b8
Jabi <michal.jabi@gmail.com>
2025-10-13 16:08:32 +0200

    explain(07): @Input() decorator to send data to the child component

--- auction-portal/src/app/header/header.component.ts
+++ auction-portal/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+interface User {
+  name: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-header',
   imports: [],
   template: `
     <header class="mb-2 p-5 bg-warning">
-      <h1>{{ appTitle }}</h1>
+      <h1>{{ appTitle }} {{luckyNumber}}</h1>
+      <div> Zalogowany user: {{user.name}} {{user.lastName}} </div>
     </header>
   `,
   styles: ``
 })
 export class HeaderComponent {
-    appTitle = 'Auction Portal';
+    @Input() appTitle = 'Auction Portal';
+    @Input() luckyNumber = 900;
+
+    @Input() user: User = { name: '', lastName: ''}
 }
