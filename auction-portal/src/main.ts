e1c5f928f3f5e26101e5a8b4b23aba3f82a7a78e
Jabi <michal.jabi@gmail.com>
2025-10-13 15:52:15 +0200

    task(06): install and add bootstrap.

--- auction-portal/src/main.ts
+++ auction-portal/src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Tak też można dopinać style z innych bibliotek (ale rozwiązanie z angular.json - jest bezpieczniejsze pod względem optymalizacji css)
+// import "bootstrap/dist/css/bootstrap.css"
+
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
