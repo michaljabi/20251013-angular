e2e5a981b9f880d853b4237810fbe99d5f23026f
Jabi <michal.jabi@gmail.com>
2025-10-13 13:09:59 +0200

    explain(03): how data bind works. {} [] () <template>

--- ng-living-theory/src/app/sample-counter/sample-counter.component.ts
+++ ng-living-theory/src/app/sample-counter/sample-counter.component.ts
@@ -4,12 +4,42 @@ import { Component } from '@angular/core';
   selector: 'app-sample-counter',
   imports: [],
   template: `
-    <p>
-      sample-counter works!
-    </p>
+    <div>
+      {{ myLuckyNumber }} sample-counter works!
+      <strong>{{ myLuckyNumber }}</strong>
+      <button class="button is-danger" [title]="titleAttribute" (click)="sayHello()"> HELLO </button>
+    </div>
   `,
   styles: ``
 })
 export class SampleCounterComponent {
+    // myLuckyNumber: null | number = null;
+    myLuckyNumber = 0;
 
+    titleAttribute = 'Tooltip 🚀';
+
+    myLinks: string[] = [];
+
+    constructor() {
+      setTimeout( () => {
+        this.myLuckyNumber = 123;
+      }, 3000)
+      console.log('HELLO')
+
+      // this.myLinks.push(1);
+      // this.myLuckyNumber = 'ABS'
+    }
+
+    sayHello() {
+      alert('Witaj na mojej stronie....')
+
+      /*
+      const h1 = document.querySelector('h1');
+      if(h1) {
+        h1.click()
+      }
+      */
+
+      return 890;
+    }
 }
