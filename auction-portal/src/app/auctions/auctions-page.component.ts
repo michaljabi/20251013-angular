87ad7c4af1b5bed200859f94d35d345968dc9105
Jabi <michal.jabi@gmail.com>
2025-10-14 15:58:53 +0200

    feat(AuctionsResourceService): it works with `HttpClient`

--- auction-portal/src/app/auctions/auctions-page.component.ts
+++ auction-portal/src/app/auctions/auctions-page.component.ts
@@ -1,20 +1,19 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {JsonPipe} from '@angular/common';
 import {AuctionsResourceService} from './auctions-resource.service';
 import {AuctionCardComponent} from './auction-card/auction-card.component';
 import {AuctionItem} from './auction-item';
 
 @Component({
-  imports: [JsonPipe, AuctionCardComponent],
+  imports: [ AuctionCardComponent],
   template: `
     <section>
-      auctions-page works! {{ auctionsResourceService.getAll() | json}}
-
-      @for(item of auctions; track item.id) {
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <app-auction-card [auctionItem]="item" />
-        </div>
-      }
+      <div class="row">
+        @for(item of auctions; track item.id) {
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <app-auction-card [auctionItem]="item" />
+          </div>
+        }
+      </div>
     </section>
   `,
   styles: ``,
@@ -28,6 +27,13 @@ export class AuctionsPageComponent implements OnInit {
 
     // Jesteś w stanie sterować momentem wywołania tej metody podczas testowania komponentu.
     ngOnInit(): void {
-      this.auctions = this.auctionsResourceService.getAll();
+      this.auctionsResourceService.getAll().subscribe({
+        next: (auctions: AuctionItem[]) => {
+          this.auctions = auctions;
+        },
+        error: (err: Error) => {
+          console.error('Error', err);
+        }
+      });
     }
 }
--- auction-portal/src/app/auctions/auctions-resource.service.ts
+++ auction-portal/src/app/auctions/auctions-resource.service.ts
@@ -1,5 +1,7 @@
-import {Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {AuctionItem, AuctionItemWithoutId} from './auction-item';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 // import {Router} from '@angular/router';
 
 // Tutaj przyszykujemy sobie tzw. Stateless Service
@@ -9,18 +11,12 @@ import {AuctionItem, AuctionItemWithoutId} from './auction-item';
 })
 export class AuctionsResourceService {
 
+    private readonly httpClient = inject(HttpClient);
+    private readonly backendUrl = 'http://localhost:3000/auctions'
     // router = inject(Router);
 
-    getAll(): AuctionItem[] {
-      return [
-        {
-          id: "1",
-          title: "Części do aparatu",
-          imgUrl: "https://picsum.photos/id/36/200/200",
-          description: "Jakiś opis",
-          price: 2000
-        }
-      ];
+    getAll(): Observable<AuctionItem[]> {
+      return this.httpClient.get<AuctionItem[]>(this.backendUrl);
     }
 
     addOne(item: AuctionItemWithoutId) {
