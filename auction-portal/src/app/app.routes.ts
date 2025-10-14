dcc5abafcc20170082de2d4caab4bdb89f999a01
Jabi <michal.jabi@gmail.com>
2025-10-14 11:09:44 +0200

    task(10)[/auctions]: first page component without selector in our routing.

--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {AuctionsPageComponent} from './auctions/auctions-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // redirect z głównej do /auctions po wejściu na aplikację naszą
+  { path: '', redirectTo: '/auctions', pathMatch: 'full' },
+  // dopisz ścieżkę do /auctions
+  { path: 'auctions', component: AuctionsPageComponent },
+  // catch all sla nieobsługiwanych ścieżek (404 TODO ):
+  { path: '**', component: HeaderComponent }
+];
