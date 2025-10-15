65bbbd778773e7a8ec0e10a1bffd6ebd4099d881
Jabi <michal.jabi@gmail.com>
2025-10-15 15:59:22 +0200

    task(21): environment variables for changing base URL

--- auction-portal/src/app/auctions/auctions-resource.service.ts
+++ auction-portal/src/app/auctions/auctions-resource.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {AuctionItem, AuctionItemWithoutId} from './auction-item';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 // import {Router} from '@angular/router';
 
 // Tutaj przyszykujemy sobie tzw. Stateless Service
@@ -11,12 +12,17 @@ import {Observable} from 'rxjs';
 })
 export class AuctionsResourceService {
 
+    // DEV:
+    //my-super.domain.com -> AJAX do http://localhost:3000/....
+    // PROD:
+    //my-super.domain.com -> AJAX do my-super.domain.com/api
+
     private readonly httpClient = inject(HttpClient);
-    private readonly backendUrl = 'http://localhost:3000/auctions'
+    private readonly auctionEndpoint = `${environment.backendBaseUrl}/auctions`
     // router = inject(Router);
 
     getAll(): Observable<AuctionItem[]> {
-      return this.httpClient.get<AuctionItem[]>(this.backendUrl);
+      return this.httpClient.get<AuctionItem[]>(this.auctionEndpoint);
     }
 
     addOne(item: AuctionItemWithoutId) {
