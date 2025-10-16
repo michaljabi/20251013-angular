833629500d5c07c598fa0edd43e23e0c064e6df8
Jabi <michal.jabi@gmail.com>
2025-10-16 10:36:02 +0200

    task(24): All validations on the form.

--- auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
+++ auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
@@ -1,28 +1,39 @@
 import {Component, inject} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-add-auction-page',
-  imports: [SharedModule],
+  imports: [SharedModule, ReactiveFormsModule, JsonPipe],
   template: `
     <section class="mt-2 row">
       <h2 class="my-4">Dodaj nową aukcję</h2>
       <div class="col-6">
-        <img class="img-thumbnail" alt="Podgląd fotografii" [src]="'https://picsum.photos/id/1/600/600'" />
+        <img class="img-thumbnail" alt="Podgląd fotografii" [src]="imgUrl" />
       </div>
       <div class="col-6">
-        <form>
+        <form [formGroup]="auctionForm" (submit)="handleSubmit()">
           <div class="form-group">
-            <label for="auctionTitle">Nazwa aukcji</label>
+            <label for="auctionTitle">Nazwa aukcji *</label>
             <div class="input-group mb-3">
               <div class="input-group-prepend">
-            <span class="input-group-text">
-              <fa-icon icon="edit"></fa-icon>
-            </span>
+              <span class="input-group-text">
+                <fa-icon icon="edit"></fa-icon>
+              </span>
               </div>
-              <input class="form-control" id="auctionTitle" type="text" name="title" />
+              <input class="form-control" id="auctionTitle" type="text" formControlName="title" />
             </div>
+<!--            {{ auctionForm.controls.title.touched }}-->
+            @let myTitle = auctionForm.controls.title;
+
+            @if(myTitle.touched && title.invalid) {
+              <div class="alert alert-danger">
+                {{ title.errors?.['required'] ? 'Pole jest wymagane' : '' }}
+                {{ title.errors?.['minlength'] ? 'Minimalna nazwa aukcji musi zawierać ' + title.errors?.['minlength']['requiredLength'] + ' znaków' : '' }}
+              </div>
+            }
+
           </div>
           <div class="form-group">
             <label for="auctionPrice">Cena aukcji</label>
@@ -32,10 +43,16 @@ import {FormBuilder} from '@angular/forms';
               <fa-icon icon="tag"></fa-icon>
             </span>
               </div>
-              <input class="form-control" id="auctionPrice" type="number" name="price" />
+              <input class="form-control" id="auctionPrice" type="number" formControlName="price" />
             </div>
+            @let price = auctionForm.controls.price;
+            @if(price.touched && price.invalid) {
+              <div class="alert alert-danger">
+                {{ price.errors?.['min'] ? 'Minimalna cena to '+ price.errors?.['min']['min'] +' zł' : '' }}
+                {{ price.errors?.['max'] ? 'Maksymalna cena to '+ + price.errors?.['max']['max']  +' zł' : '' }}
+              </div>
+            }
           </div>
-
           <div class="form-group">
             <label for="img">Zdjecie</label>
             <div class="input-group mb-3">
@@ -44,18 +61,22 @@ import {FormBuilder} from '@angular/forms';
               <fa-icon icon="image"></fa-icon>
             </span>
               </div>
-              <input class="form-control" id="img" type="number" name="imgId" />
+              <input class="form-control" id="img" type="number" formControlName="imgId" />
             </div>
+            @if( auctionForm.controls.imgId.touched && auctionForm.controls.imgId.invalid) {
+              <div class="alert alert-danger">
+                {{ auctionForm.controls.imgId.errors | json }}
+              </div>
+            }
           </div>
-
           <div class="form-group">
             <label for="auctionDescription">Szczegółowy opis</label>
             <div class="input-group mb-3">
-              <textarea class="form-control" id="auctionDescription" rows="5" name="description"></textarea>
+              <textarea class="form-control" id="auctionDescription" rows="5" formControlName="description"></textarea>
             </div>
           </div>
           <div class="text-right">
-            <button class="btn btn-primary" type="submit">
+            <button class="btn btn-primary" type="submit" [style.opacity]="auctionForm.invalid ? 0.6 : 1">
               <fa-icon icon="gavel"></fa-icon> Dodaj aukcję
             </button>
           </div>
@@ -66,12 +87,34 @@ import {FormBuilder} from '@angular/forms';
   styles: `
     input.ng-invalid.ng-touched,
     textarea.ng-invalid.ng-touched {
-      border: 1px solid #dc3545;
+      border: 2px solid #dc3545;
     }
   `
 })
 export class AddAuctionPageComponent {
     private fb = inject(FormBuilder);
 
-    auctionForm = this.fb.group({})
+    auctionForm = this.fb.group({
+      // title: new FormControl('??'),
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      price: [0, [Validators.required, Validators.min(10), Validators.max(10_000_000)]],
+      imgId: [12, [Validators.min(0), Validators.max(1084)]],
+      description: ['']
+    })
+
+    get title() {
+      return this.auctionForm.controls.title
+    }
+
+    get imgUrl() {
+      return `https://picsum.photos/id/${this.auctionForm.value.imgId}/600/600`
+    }
+
+    handleSubmit() {
+      if(this.auctionForm.invalid) {
+        this.auctionForm.markAllAsTouched();
+        return;
+      }
+      console.log('Wartości', this.auctionForm.value)
+    }
 }
