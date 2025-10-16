36a3c29d657f1ee42e36cf99879f3ae0a7385438
Jabi <michal.jabi@gmail.com>
2025-10-16 09:19:33 +0200

    task(22): add form template + FromBuilder DI.

--- auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
+++ auction-portal/src/app/auctions/add-auction-page/add-auction-page.component.ts
@@ -1,15 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {SharedModule} from '../../shared/shared.module';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-add-auction-page',
-  imports: [],
+  imports: [SharedModule],
   template: `
-    <p>
-      add-auction-page works!
-    </p>
+    <section class="mt-2 row">
+      <h2 class="my-4">Dodaj nową aukcję</h2>
+      <div class="col-6">
+        <img class="img-thumbnail" alt="Podgląd fotografii" [src]="'https://picsum.photos/id/1/600/600'" />
+      </div>
+      <div class="col-6">
+        <form>
+          <div class="form-group">
+            <label for="auctionTitle">Nazwa aukcji</label>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+            <span class="input-group-text">
+              <fa-icon icon="edit"></fa-icon>
+            </span>
+              </div>
+              <input class="form-control" id="auctionTitle" type="text" name="title" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="auctionPrice">Cena aukcji</label>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+            <span class="input-group-text">
+              <fa-icon icon="tag"></fa-icon>
+            </span>
+              </div>
+              <input class="form-control" id="auctionPrice" type="number" name="price" />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="img">Zdjecie</label>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+            <span class="input-group-text">
+              <fa-icon icon="image"></fa-icon>
+            </span>
+              </div>
+              <input class="form-control" id="img" type="number" name="imgId" />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="auctionDescription">Szczegółowy opis</label>
+            <div class="input-group mb-3">
+              <textarea class="form-control" id="auctionDescription" rows="5" name="description"></textarea>
+            </div>
+          </div>
+          <div class="text-right">
+            <button class="btn btn-primary" type="submit">
+              <fa-icon icon="gavel"></fa-icon> Dodaj aukcję
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   `,
-  styles: ``
+  styles: `
+    input.ng-invalid.ng-touched,
+    textarea.ng-invalid.ng-touched {
+      border: 1px solid #dc3545;
+    }
+  `
 })
 export class AddAuctionPageComponent {
+    private fb = inject(FormBuilder);
 
+    auctionForm = this.fb.group({})
 }
